a38a0d8a342cf9b7e369a7cc40b29baaf8ca5976
prasanta.ani <prasanta.ani@gmail.com>
2026-01-07 18:29:19 +0000

    upd

--- src/app/miscFunctions/arduinoCode.js
+++ src/app/miscFunctions/arduinoCode.js
@@ -43,153 +43,186 @@ void loop(){
 
 
 const esp8266Code = `
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClientSecureBearSSL.h>
+#include <WiFiUdp.h>
+#include <Preferences.h>
 
-// Add your WiFi credentials
-const char* mySSID = "";
-const char* myPASSWORD = "";
+#include <ArduinoOTA.h>
+#include <Arduino_JSON.h>
+#include <TelnetStream.h>
 
-WiFiUDP ntpUDP;
-NTPClient timeClient(ntpUDP);
+const char* mySSID = "PK_NEO";
+const char* myPASSWORD = "cxa1619bs";
 
 Preferences preferences;
+
+String FB_Email = "abc@xyz.com";
+String FB_Password = "123456";
+String deviceName = "NodeMCU";
 
 //Built-in LED D0(GPIO16) or D4(GPIO2)
 #define LED_PIN D4
 
-std::unique_ptr<BearSSL::WiFiClientSecure> client(new BearSSL::WiFiClientSecure);
+String authCode, devCode, httpRes;
 
-String authCode, deviceName, devCode, httpRes;
+class HttpReq {
 
-void setup() {
+private:
+
+  String host = "https://hommily.vercel.app/api/";
+  JSONVar httpResponse;
+
+
+  String uid = preferences.getString("uid", "NONE");
+  String devCode = preferences.getString("devCode", "NONE");
+
+
+  void httpRequest(String reqMethode, String PATH, String data) {
+
+    std::unique_ptr<BearSSL::WiFiClientSecure> client(new BearSSL::WiFiClientSecure);
+    HTTPClient http;
+
+    if (client) {
+      client->setInsecure();
+      HTTPClient https;
+
+      if (https.begin(*client, this->host + PATH)) {
+
+        int httpCode;
+
+        if (reqMethode == "POST") {
+          https.addHeader("Content-Type", "application/json");
+          httpCode = https.POST(data);
+        } else if (reqMethode == "PATCH") {
+          https.addHeader("Content-Type", "application/json");
+          httpCode = https.PATCH(data);
+        } else {
+          httpCode = https.GET();
+        }
+
+
+        if (httpCode > 0) {
+          String httpRes = https.getString();
+          this->httpResponse = JSON.parse(httpRes);
+
+          if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
+            Serial.print("==>  ");
+            Serial.println(httpRes);
+          } else if (httpCode == HTTP_CODE_UNAUTHORIZED) {
+            Serial.println(httpRes);
+          } else {
+            Serial.println(httpRes);
+          }
+        } else {
+          Serial.println("HTTP ERROR");
+          httpRes = "HTTP_ERROR";
+        }
+        https.end();
+      }
+    }
+  }
+
+
+public:
+
+  void initDevice() {
+    Serial.printf("UID: %s", this->uid);
+
+    if (this->uid == "NONE") {
+      JSONVar data;
+      data["email"] = FB_Email;
+      data["password"] = FB_Password;
+
+      Serial.println(data);
+      this->httpRequest("POST", "arduinoSignIn", JSON.stringify(data));
+
+      this->uid = String(this->httpResponse["uid"]);
+      Serial.print(this->uid);
+      Serial.println();
+
+      if (this->uid == "NONE") {
+        Serial.println("Wrong Credential");
+        while (true) {
+          digitalWrite(LED_PIN, !digitalRead(LED_PIN));
+          delay(500);
+        }
+      } else {
+        Serial.println("Sign in Successfull");
+
+        this->devCode = this->genRandString(8);
+        preferences.putString("uid", this->uid);
+        preferences.putString("devCode", this->devCode);
+
+
+        JSONVar devCreate;
+        devCreate["uid"] = this->uid;
+        devCreate["path"] = this->uid;
+        devCreate[this->devCode]["name"] = deviceName;
+        devCreate[this->devCode]["deviceCode"] = this->devCode;
+
+        this->httpRequest("POST", "setArduinoData", JSON.stringify(devCreate));
+      }
+    }
+  }
+
+
+
+  void sendData(String FEED, String VALUE) {
+
+    JSONVar data;
+
+
+    data["uid"] = this->uid;
+    data["path"] = this->devCode + "/devFeeds";
+    data["data"][FEED] = VALUE;
+
+    Serial.println(data);
+
+    // httpRequest(String httpMethod, String URL, JSONVar data)
+    this->httpRequest("POST", "setArduinoData", JSON.stringify(data));
+  }
+
+  String genRandString(int digit) {
+
+    String rndString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    String rndGenString;
+
+    for (int i = 0; i < digit; i++) {
+      rndGenString += rndString.charAt(random(rndString.length()));
+    }
+
+    return rndGenString;
+  }
+};
+
+HttpReq EQ;
+
+void setup(void) {
+
   Serial.begin(115200);
   setupWifiOTA();
 
   preferences.begin("device", false);
-  timeClient.begin();
+  preferences.clear();
+  EQ.initDevice();
+}
 
-  setUpDevice();
-};
 
 void myTask() {
-  sendData("Temperature", String(random(10, 50)));
-  sendData("Random", genRandString(4));
+  // sendData("Temperature", String(random(10, 50)));
+  // sendData("Random", genRandString(4));
 }
 
 
 
 void loop() {
-  timeClient.update();
   // ===============Only For OTA===============
   ArduinoOTA.handle();
   // ===============Only For OTA===============
 
-  refreshData();
   myTask();
-}
-
-void refreshData() {
-
-  sendRequest("GET", host + "/" + uid + "/" + devCode + ".json?auth=" + authCode, "");
-
-  String webDevName = String(JSON.parse(httpRes)["name"]);
-
-  if (webDevName != deviceName) {
-    preferences.putString("devName", webDevName);
-  };
-
-  if (JSON.parse(httpRes)["isDeleted"]) {
-    sendRequest("PATCH", host + "/" + uid + "/" + devCode + ".json?auth=" + authCode, "null");
-    preferences.clear();
-    ESP.restart();
-  }
-}
-
-
-void setUpDevice() {
-  authCode = preferences.getString("auth", "NONE");
-  deviceName = preferences.getString("devName", "NONE");
-  devCode = preferences.getString("devCode", "NONE");
-
-  if (authCode == "NONE") {
-
-    devCode = genRandString(8);
-
-    JSONVar devObj;
-
-    devObj[devCode]["uid"] = uid;
-    devObj[devCode]["deviceCode"] = devCode;
-    devObj[devCode]["deviceName"] = "Node MCU";
-
-    sendRequest("PATCH", host + "/device.json", JSON.stringify(devObj));
-
-    while (true) {
-
-      sendRequest("GET", host + "/device/" + devCode + ".json", "");
-
-      authCode = String(JSON.parse(httpRes)["authCode"]);
-      deviceName = String(JSON.parse(httpRes)["deviceName"]);
-
-
-      if (authCode == "") {
-        Serial.println("Add Device on Portal");
-      } else {
-        Serial.println(authCode);
-        Serial.println(deviceName);
-        preferences.putString("devName", deviceName);
-        preferences.putString("auth", authCode);
-        preferences.putString("devCode", devCode);
-
-        sendRequest("PATCH", host + "/device/" + devCode + ".json", "null");
-        displayConfig();
-        break;
-      }
-    }
-  } else {
-    displayConfig();
-  }
-
-  deviceConfig();
-}
-
-
-void deviceConfig() {
-
-  JSONVar devObj;
-
-  devObj["deviceCode"] = devCode;
-  devObj["name"] = deviceName;
-
-  String pathName = host + "/" + uid + "/" + devCode + ".json?auth=" + authCode;
-
-  sendRequest("PATCH", pathName, JSON.stringify(devObj));
-}
-
-
-void sendData(String feedName, String feedValue) {
-
-  JSONVar data;
-
-  data["value"] = feedValue;
-  data["time"] = String(timeClient.getEpochTime());
-  String pathName = host + "/" + uid + "/" + devCode + "/" + "devFeeds/" + feedName + ".json?auth=" + authCode;
-
-  sendRequest("PATCH", pathName, JSON.stringify(data));
-}
-
-void displayConfig() {
-
-  authCode = preferences.getString("auth", "NONE");
-  deviceName = preferences.getString("devName", "NONE");
-
-
-  Serial.print("UID ==>   ");
-  Serial.println(uid);
-  Serial.print("Auth Code ==>   ");
-  Serial.println(authCode);
-  Serial.print("Device Name ==>   ");
-  Serial.println(deviceName);
-  Serial.print("Device Code ==>   ");
-  Serial.println(devCode);
 }
 
 
@@ -201,9 +234,11 @@ void setupWifiOTA() {
     Serial.println("Connection Failed! Rebooting...");
     delay(10000);
     ESP.restart();
+    // WiFi.softAP("NODE_MCU", "1234567890");
+    // break;
   }
 
-  ArduinoOTA.setHostname("NODE_MCU");
+  ArduinoOTA.setHostname("PK_NODE_MCU");
   // ArduinoOTA.setPassword("cxa1619bs");
 
   ArduinoOTA.onStart([]() {
@@ -218,10 +253,10 @@ void setupWifiOTA() {
     Serial.println("Start updating " + type);
   });
   ArduinoOTA.onEnd([]() {
-    Serial.println("End");
+    Serial.println("\nEnd");
   });
   ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-    Serial.printf("Progress: %u%%", (progress / (total / 100)));
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
   });
   ArduinoOTA.onError([](ota_error_t error) {
     Serial.printf("Error[%u]: ", error);
@@ -244,59 +279,7 @@ void setupWifiOTA() {
   Serial.println(WiFi.localIP());
 }
 
-void sendRequest(String reqMethode, String PATH, String data) {
 
-  HTTPClient http;
-
-  if (client) {
-    client->setInsecure();
-    HTTPClient https;
-
-    if (https.begin(*client, PATH)) {
-
-      int httpCode;
-
-      if (reqMethode == "POST") {
-        https.addHeader("Content-Type", "application/json");
-        httpCode = https.POST(data);
-      } else if (reqMethode == "PATCH") {
-        https.addHeader("Content-Type", "application/json");
-        httpCode = https.PATCH(data);
-      } else {
-        httpCode = https.GET();
-      }
-
-      if (httpCode > 0) {
-        httpRes = https.getString();
-
-        if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
-          Serial.print("==>  ");
-          Serial.println(httpRes);
-        } else if (httpCode == HTTP_CODE_UNAUTHORIZED) {
-          Serial.println(httpRes);
-        } else {
-          Serial.println(httpRes);
-        }
-      } else {
-        Serial.println("HTTP ERROR");
-        httpRes = "HTTP_ERROR";
-      }
-      https.end();
-    }
-  }
-}
-
-String genRandString(int digit) {
-
-  String rndString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-  String rndGenString;
-
-  for (int i = 0; i < digit; i++) {
-    rndGenString += rndString.charAt(random(rndString.length()));
-  }
-
-  return rndGenString;
-}
 `;
 
 
