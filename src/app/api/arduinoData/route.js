7475a5a1344ca9cbef645b33f0d876750a8b0e44
prasanta.ani <prasanta.ani@gmail.com>
2026-01-07 07:48:37 +0000

    upd

--- src/app/api/arduinoData/route.js
+++ src/app/api/arduinoData/route.js
@@ -1,17 +1,23 @@
 import { NextResponse } from 'next/server';
-import { db } from '../../firebase/config'
-import { ref, get } from "firebase/database"
+import admin from '../../firebase/adminConfig.js';
 
 export async function POST(request) {
     try {
         const bodyData = await request.json();
         const { path } = bodyData;
+        try {
+            await admin.auth().getUser(path);
 
-        const dbRef = ref(db, path);
-        const snapshot = await get(dbRef);
-        const data = snapshot.val();
-
-        return NextResponse.json(data, { status: 200 });
+            const db = admin.database();
+            const ref = db.ref(path);
+            const snapshot = await ref.once('value');
+            const data = snapshot.val();
+            
+            return NextResponse.json(data, { status: 200 });
+        }
+        catch {
+            return NextResponse.json({ "error": "User Not Found" }, { status: 200 });
+        }
     } catch (error) {
         console.error('Error fetching data:', error);
         return NextResponse.json({ error: 'Failed to fetch data' }, { status: 500 });
