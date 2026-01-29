4b927f6c42000a45bd8706e27b0756fbeab51ccc
prasanta1987 <prasanta.1987@hotmail.com>
2026-01-29 12:47:56 +0530

    feed timestamp added

--- src/app/feeds/ui/FeedCard.jsx
+++ src/app/feeds/ui/FeedCard.jsx
@@ -43,7 +43,11 @@ export default function FeedCard({ feed, boardName, feedName, deviceCode, uid, t
 
     const sliderValueChange = (e) => {
         console.log(e.target.value);
-        updateValuesToDatabase(`${uid}/${deviceCode}/devFeeds/${feedName}`, { value: e.target.value });
+        updateValuesToDatabase(`${uid}/${deviceCode}/devFeeds/${feedName}`, 
+            { 
+                value: e.target.value, 
+                time: new Date().getTime()
+            });
     }
 
     return (
