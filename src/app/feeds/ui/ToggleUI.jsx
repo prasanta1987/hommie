d278a74d9c617f0e1535bca5440b0f1ad3bb0a08
prasanta1987 <prasanta.1987@hotmail.com>
2026-01-29 12:34:49 +0530

    toggle button style changed

--- src/app/feeds/ui/ToggleUI.jsx
+++ src/app/feeds/ui/ToggleUI.jsx
@@ -2,17 +2,14 @@ import React from 'react';
 
 const ToggleUI = ({ value, onChange, onMouseUp, onTouchEnd }) => {
     return (
-        <div className='w-100'>
-            <div className="feed-value">{value}</div>
-            <div className="d-flex flex-column w-100 justify-content-between align-items-center">
-                <input
-                    type="checkbox"
-                    checked={value}
-                    className="feed-toggler"
-                    onChange={(e) => onChange(e.target.checked)}
-                />
-                <span>{value ? 'ON' : 'OFF'}</span>
-            </div>
+        <div className='toggle-switch'>
+            <input
+                type="checkbox"
+                checked={value}
+                className="feed-toggler"
+                onChange={(e) => onChange(e.target.checked)}
+            />
+            <span className="slider"></span>
         </div>
     );
 };
