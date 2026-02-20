1839e8551918e0fc2f3feed61b3a8c0803e1fddc
prasanta1987 <prasanta.1987@hotmail.com>
2026-02-20 16:48:27 +0530

    create feed modal seperated

--- src/app/feeds/components/LandingPage.jsx
+++ src/app/feeds/components/LandingPage.jsx
@@ -3,10 +3,10 @@
 import React, { useState, useEffect } from 'react';
 import Boards from './Boards';
 import Feeds from './Feeds';
-import FeedModal from '../ui/FeedModal';
 import { FiPlus } from 'react-icons/fi';
+import FeedCreateModal from '@/app/feeds/ui/FeedCreateModal';
 
-import { updateValuesToDatabase } from '../../miscFunctions/actions';
+import { updateValuesToDatabase } from '@/app/miscFunctions/actions';
 import './LandingPage.css'
 
 const LandingPage = (props) => {
@@ -15,16 +15,6 @@ const LandingPage = (props) => {
   const [selectedDeviceCode, setSelectedDeviceCode] = useState('');
   const [devices, setDevices] = useState([]);
   const [showModal, setShowModal] = useState(false);
-  const [feedName, setFeedName] = useState('');
-  const [feedType, setFeedType] = useState('Card');
-  const [minValue, setMinValue] = useState(0);
-  const [maxValue, setMaxValue] = useState(100);
-  const [mcuType, setMcuType] = useState('ESP32');
-  const [GPIO, setGPIO] = useState(0);
-  const [rPIN, setRPIN] = useState(0);
-  const [gPIN, setGPIN] = useState(0);
-  const [bPIN, setBPIN] = useState(0);
-
 
   const boardSelection = (devCode, devFeed) => {
     const feedStatus = dbData[devCode].devFeeds[devFeed].isSelected;
@@ -54,59 +44,6 @@ const LandingPage = (props) => {
       setSelectedDeviceCode(deviceList[0].deviceCode);
     }
   }, [dbData]);
-
-  const handleCreateFeed = () => {
-
-    if (!feedName) {
-      alert("Feed name is required.");
-      return;
-    }
-    const reference = `${props.userData.uid}/${selectedDeviceCode}/devFeeds/${feedName}`;
-    const newFeed = {
-      type: feedType,
-      value: 0,
-      time: new Date().getTime(),
-      isSelected: true
-    };
-
-    if (feedType === 'Gauge' || feedType === 'Slider') {
-      newFeed.rangeMin = parseFloat(minValue);
-      newFeed.rangeMax = parseFloat(maxValue);
-    }
-
-    if (feedType === 'Toggle') {
-      if (GPIO == 0) {
-        alert("GPIO must be a number.");
-        return;
-      }
-      newFeed.GPIO = parseInt(GPIO);
-      newFeed.value = 0;
-      newFeed.mcu = mcuType;
-    }
-
-    if (feedType === 'Colour') {
-      if (rPIN == 0 || gPIN == 0 || bPIN == 0) {
-        alert("PINs must be numbers.");
-        return;
-      }
-      newFeed.rPIN = parseInt(rPIN);
-      newFeed.gPIN = parseInt(gPIN);
-      newFeed.bPIN = parseInt(bPIN);
-      newFeed.mcu = mcuType;
-      newFeed.value = "#2576b9";
-    }
-
-    updateValuesToDatabase(reference, newFeed);
-
-    // Reset form fields
-    setSelectedDeviceCode(devices.length > 0 ? devices[0].deviceCode : '');
-
-    setFeedName('');
-    setFeedType('Card');
-    setMinValue(0);
-    setMaxValue(100);
-    setShowModal(false);
-  }
 
 
   return (
@@ -141,32 +78,14 @@ const LandingPage = (props) => {
         <FiPlus size={24} />
       </button>
 
-      <FeedModal
-        purpose={"create"}
+      <FeedCreateModal
         isOpen={showModal}
-        onClose={() => setShowModal(false)}
-        feedName={feedName}
-        setFeedName={setFeedName}
-        selectedDeviceCode={selectedDeviceCode}
+        setShowModal={() => setShowModal(false)}
         devices={devices}
-        feedType={feedType}
-        setFeedType={setFeedType}
-        minValue={minValue}
-        setMinValue={setMinValue}
-        maxValue={maxValue}
-        setMaxValue={setMaxValue}
-        handleCreateFeed={handleCreateFeed}
-        setGPIO={setGPIO}
-        setSelectedDeviceCode={setSelectedDeviceCode}
-        mcuType={mcuType}
-        setMcuType={setMcuType}
-        rPIN={rPIN}
-        gPIN={gPIN}
-        bPIN={bPIN}
-        setRPIN={setRPIN}
-        setGPIN={setGPIN}
-        setBPIN={setBPIN}
+        uid={props.userData.uid}
+        selectedDeviceCode={selectedDeviceCode}
       />
+
     </div>
 
   );
